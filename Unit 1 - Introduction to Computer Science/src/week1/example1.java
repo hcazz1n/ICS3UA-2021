b44c9f6f61dc1ac17e6581be3700724d5a3bfa5e
Harrison Cazzin <hcazzin@bayviewglen.ca>
2021-09-09 13:15:09 -0400

    Unit 1 First Commit

---
+++ Unit 1 - Introduction to Computer Science/src/week1/example1.java
@@ -0,0 +1,8 @@
+package week1;
+
+public class example1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to ICS3U-AP"); //System.out.println displays text to screen on new line
+    }
+
+}
---
+++ Unit 1 - Introduction to Computer Science/src/week1/example2.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class example2 {
+   public static void main(String[] args) {
+       System.out.println("A"); //prints A and hits enter
+       System.out.print("B");   //prints B
+       System.out.print("C");   //prints C
+       System.out.println("D"); //prints D and hits enter
+       System.out.println("E"); //prints E and hits enter
+   } 
+}
