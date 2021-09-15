29f14c24b03a36a2f86316689330531fbb16f3fc
Harrison Cazzin <hcazzin@bayviewglen.ca>
2021-09-15 13:44:32 -0400

    Student class u2

---
+++ Unit 2 - Using Objects/src/week2/StudentExample.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        //richie is a variable to an instance of the class Student
+        //richie is an Object (instances of a class)
+        Student richie = new Student("Richie", "69420", 12);
+    }
+}
