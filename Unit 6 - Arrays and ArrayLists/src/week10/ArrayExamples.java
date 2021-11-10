1305f9650abfe546b47c5907c7f567fb164dd9a2
Harrison Cazzin <hcazzin@bayviewglen.ca>
2021-11-09 19:23:09 -0500

    arrayexamples

--- Unit 6 - Arrays and ArrayLists/src/week10/ArrayExamples.java
+++ Unit 6 - Arrays and ArrayLists/src/week10/ArrayExamples.java
@@ -4,6 +4,26 @@ public class ArrayExamples {
     public static void main(String[] args) {
         primitiveArray();
         arrayObjectExample();
+        iterateArrayExample();
+        int[] arr = {9, 8, 7, 6, 5};
+        mystery(arr);
+    }
+
+    private static void mystery(int[] arr) {
+        int index = (int)(Math.random() * arr.length);
+        arr[index] = -arr[index];
+    }
+
+    private static void iterateArrayExample() {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int numOdd = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 == 1){
+                numOdd++;
+            }
+        }
     }
 
     private static void arrayObjectExample() {
